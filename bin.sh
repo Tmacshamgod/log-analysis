910bbcc44330d570b7353079ab78f8c0cbbe7163
Jeff Zhang <jeffzhang@cloutropy.com>
2016-06-30 15:00:41 +0800

    remove duplicate log record

--- bin.sh
+++ bin.sh
@@ -3,7 +3,7 @@ dd=$(date -v -1d '+%Y-%m-%d')
 latest_file=`ssh admin@$1 find $2 -type f -daystart -mtime 1`
 for i in $latest_file; do
     scp admin@$1:$i $PWD 
-    cat -n $PWD'/'${i##*/} | grep --line-buffered -i -E "err|warn" > $PWD'/tornado/project/statics/files/'$1'.'$dd'.'${i##*/}
+    cat -n $PWD'/'${i##*/} | grep --line-buffered -i -E "err|warn" | sort -u > $PWD'/tornado/project/statics/files/'$1'.'$dd'.'${i##*/}
     rm $PWD'/'${i##*/}
 done
 echo Completed.
